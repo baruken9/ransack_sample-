0b0dec53e660661d33d1078a3700b1bbf0896d7d
桃原健太 <>
2020-03-11 16:29:58 +0900

    Rails課題６になります。

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  include ActiveModel::Model
+  attr_accessor :search_name, :search_age
+
+  def execute
+    User.ransack(name_eq: @search_name, age_eq: @search_age).result
+  end
+end
